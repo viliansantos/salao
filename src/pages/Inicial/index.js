32a20974bbd133720c9e6cd7f87cd1a530752f36
Vilian Santos <53971081+viliansantos@users.noreply.github.com>
2020-04-20 14:32:53 -0300

    Definição final das rotas e estrutura da aplicação

---
+++ src/pages/Inicial/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Inicial() {
+    return (
+
+        <h1>Componente inicial</h1>
+    );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+
+import Inicial from './pages/Inicial';
+import Sobre from './pages/Sobre';
 import Depoimentos from './pages/Depoimentos';
 import Servicos from './pages/Servicos';
 import Contato from './pages/Contato';
@@ -9,7 +12,9 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Depoimentos} />
+                <Route path="/" exact component={Inicial} />
+                <Route path="/sobre" component={Sobre} />
+                <Route path="/depoimentos" component={Depoimentos} />
                 <Route path="/servicos" component={Servicos} />
                 <Route path="/contato" component={Contato} />
             </Switch>
