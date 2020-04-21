a5b3a1a210db3bf540d9aea24d93b950cc5b9d8c
Vilian Santos <53971081+viliansantos@users.noreply.github.com>
2020-04-21 10:56:21 -0300

    Alteração no componente Inicial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import './global.css';
+
 import Routes from './routes'
 
 export default function App() {
--- src/pages/Inicial/index.js
+++ src/pages/Inicial/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import '../../global.css';
+import './style.css';
+
+import logoImg from '../../assets/NegaRaiz.svg';
+
 export default function Inicial() {
     return (
+        <div className="inicial-container">
 
-        <h1>Componente inicial</h1>
+
+            <div className="menu">
+                <section>
+
+                    <img src={logoImg} alt="NegaRaiz"/>
+                    
+                </section>
+
+                <h1>Serviços</h1>
+                <h1>Sobre</h1>
+                <h1>Contato</h1>
+            </div>
+
+
+            <div className="content">
+
+                <section>
+
+                    <h1>Seu cabelo fala por você</h1>
+
+                </section>
+
+
+                <button className="button" type="submit">Depoimentos</button>
+
+            </div>
+
+
+
+        </div>
     );
 }
